98909ae46fa3d35ffc594aa688c1ca462cc945d4
David Stancu <dstancu@nyu.edu>
2020-01-11 23:10:27 -0500

    basic gradle skel + a small shell to start

---
+++ src/main/java/suggest/App.java
@@ -0,0 +1,9 @@
+package suggest;
+
+import suggest.Shell;
+
+public class App {
+  public static void main(String[] args) {
+    new Shell().run();
+  }
+}
---
+++ src/main/java/suggest/Shell.java
@@ -0,0 +1,83 @@
+package suggest;
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.Arrays;
+
+/**
+ * Parse commands from stdin
+ */
+public final class Shell {
+  
+  /**
+   * Basic data class for command entries
+   */
+  private final class ShellCommandEntry {
+    public final String description;
+    public final Consumer<String[]> handler;
+
+    ShellCommandEntry(
+      final String description,
+      final Consumer<String[]> handler
+    ) {
+      this.description = description;
+      this.handler = handler;
+    }
+  }
+
+  private static Scanner scanner = new Scanner(System.in);
+  private final Map<String, ShellCommandEntry> commands = new HashMap<String, ShellCommandEntry>();
+
+  Shell() {
+    this.commands.put("help", new ShellCommandEntry(
+      "Print list of commands",
+      this::showHelp
+    ));
+  }
+
+  public void run() {
+    while (true) {
+      System.out.print("suggest> ");
+      if (!scanner.hasNextLine()) { break; }
+
+      final String[] args = scanner.nextLine().split(" ");
+      final Optional<ShellCommandEntry> entry = Optional.ofNullable(this.commands.get(args[0]));
+
+      if (entry.isPresent()) {
+        entry.get().handler.accept(Arrays.copyOfRange(args, 1, args.length));
+      } else {
+        System.out.printf("Invalid command %s; try help.\n", args[0]);
+      }
+    }
+  }
+
+  /**
+   * Add a command by name with handler
+   * 
+   * @param commandName
+   * @param handler
+   * @return
+   */
+  public Shell bindCommand(final String commandName, final String description, final Consumer<String[]> handler) {
+    if (commandName.equalsIgnoreCase("help")) {
+      throw new IllegalArgumentException("'help' is a reserved command name");
+    }
+
+    this.commands.put(
+      commandName.toLowerCase(), 
+      new ShellCommandEntry(description, handler)
+    );
+
+    return this;
+  }
+
+  private void showHelp(String[] _args) {
+    this.commands.forEach(
+      (String command, ShellCommandEntry entry) ->
+        System.out.printf("%s -- %s\n", command, entry.description)
+    );
+  }
+}
